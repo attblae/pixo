403a26528508a857fbbaf1c6ed0ffd6ce95bd5af
Гнетова Надежда Сергеевна <GnetovaNS@stud.rosnou.ru>
2026-01-26 10:43:07 +0300

    feat:new python file

---
+++ main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static/"))
+
+
+# database = [
+#     {
+#         "id": 1,
+#         "username": "test",
+#         "age": 25
+#     },
+#     {
+#         "id": 2,
+#         "username": "tes2t",
+#         "age": 22
+#     }
+# ]
+
+
+@app.get("/")
+def root():
+    return FileResponse("pages/main.html")
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    # 127.0.0.1
+    # 0.0.0.0
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=8657,
+        reload=True
+    )
