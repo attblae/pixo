3d8888d3ba599c71366846eed1173d535c7bdb2c
unknown <daniil.d.maksimov@gmail.com>
2026-02-02 20:08:53 +0300

    feat: complet the create page

--- create_tables.py
+++ create_tables.py
@@ -4,9 +4,9 @@ def users_base(con, cursor):
     cursor.execute(
     """
         CREATE TABLE if not exists users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,  
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             username VARCHAR(30) UNIQUE,
-            password VARCHAR(255), 
+            password VARCHAR(255),
             name VARCHAR(35),
             surname VARCHAR(35),
             patronymic VARCHAR(35),
